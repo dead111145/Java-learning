217709bb4d2a70bb8b2ca21cdb1487d345e26541
dead111145 <dead111145@gmail.com>
2016-12-28 15:52:17 -0800

    Create DrivingProvider.java

---
+++ Polymorphism/DrivingProvider.java
@@ -0,0 +1,12 @@
+/**
+ * Created by dead1 on 12/28/2016.
+ */
+public class DrivingProvider extends Company {
+    /*
+    * For Driving Provider, it will have nothing special but just a constructor
+    * */
+
+    public DrivingProvider(String name, String creator, int size, int age, double earning) {
+        super(name, creator, size, age, earning);
+    }
+}
